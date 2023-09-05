a4408420c746115ea57f28579382abdde1685e0b
fiddly-digits <rob.crlu@gmail.com>
2023-09-05 15:53:50 -0600

    feat: fieldValidation middleware created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,7 @@
-export const login = (req, res) => {
-  res.json({ ok: 'login' });
-};
-
 export const register = (req, res) => {
   console.log(req.body);
   res.json({ ok: 'register' });
+};
+export const login = (req, res) => {
+  res.json({ ok: 'login' });
 };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,14 +1,31 @@
 import express from 'express';
 import { body } from 'express-validator';
 import { login, register } from '../controllers/auth.controller.js';
+import { fieldValidation } from '../middlewares/user.middleware.js';
 
 const router = express.Router();
-
 router.post(
-  '/login',
-  [body('email', 'Formato de Email no valido').isEmail().normalizeEmail()],
-  login
+  '/register',
+  [
+    body('email', 'Formato de Email no valido')
+      .trim()
+      .isEmail()
+      .normalizeEmail(),
+    body('password', 'La contraseña debe tener al menos 6 caracteres')
+      .trim()
+      .isLength({ min: 6 })
+      .custom((value, { req }) => {
+        if (value !== req.body.confirmPassword) {
+          throw new Error('Las contraseñas no coinciden');
+        } else {
+          return value;
+        }
+      })
+  ],
+  fieldValidation,
+  register
 );
-router.post('/register', register);
+
+router.post('/login', login);
 
 export default router;
